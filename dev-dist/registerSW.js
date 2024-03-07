ea09837b6843c6006914f1a8efed208624c5f126
Nehemiah Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2024-03-06 20:56:14 -0600

    configured PWA

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/contact-card/dev-sw.js?dev-sw', { scope: '/contact-card/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate', devOptions: {
+      enabled: true
+    },
+    includeAssets: ['favicon.svg'],
+    manifest: {
+      name: 'Contact Card',
+      short_name: 'Contact-Card',
+      description: 'Build a contact card on your mobile device',
+      theme_color: '#222587',
+      icons: [
+        {
+          src: 'icon-192.svg',
+          sizes: '192x192',
+          type: 'image/svg'
+        },
+        {
+          src: 'icon-512.svg',
+          sizes: '512x512',
+          type: 'image/svg'
+        }
+      ]
+    }
+  })],
   server: {
     port: 5000,
   },
